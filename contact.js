49598a07e08435a79b4ee0226a2d9b5ee49b23a6
Azra Ally <allyazra2003@gmail.com>
2024-06-06 16:10:15 +0200

    completed all Changes

---
+++ contact.js
@@ -0,0 +1,18 @@
+window.addEventListener('scroll', function() {
+    var scrollBtn = document.getElementById('scrollBtn');
+    if (window.scrollY > 0) {
+      scrollBtn.style.opacity = 0;
+    } else {
+      scrollBtn.style.opacity = 1;
+    }
+  });
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.getElementById('hamburger');
+    const navUL = document.getElementById('nav-ul');
+
+    hamburger.addEventListener('click', () => {
+        navUL.classList.toggle('show');
+    });
+});
